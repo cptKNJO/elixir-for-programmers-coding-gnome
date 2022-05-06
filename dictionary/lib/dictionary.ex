913d80a4fa449b5803e3d102be4463fcc52656eb
Kazi <kazinoshin.jo@gmail.com>
2022-05-07 04:52:20 +0600

    Add types

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -1,7 +1,7 @@
 defmodule Dictionary do
   alias Dictionary.Impl.Words
 
-  defdelegate start, to: Words
+  defdelegate start, to: Words, as: :word_list
 
   defdelegate random_word(word_list), to: Words
 end
--- dictionary/lib/impl/words.ex
+++ dictionary/lib/impl/words.ex
@@ -1,19 +1,19 @@
 defmodule Dictionary.Impl.Words do
-  def start() do
-    word_list()
-    |> random_word()
-  end
+  @type t :: list(String.t())
 
+  @spec word_list() :: t
   def word_list() do
     "assets/words.txt"
     |> get_words_from_file()
   end
 
+  @spec random_word(t) :: String.t()
   def random_word(word_list) do
     word_list
     |> Enum.random()
   end
 
+  @spec get_words_from_file(String.t()) :: t
   defp get_words_from_file(filename) do
     filename
     |> File.read!()
