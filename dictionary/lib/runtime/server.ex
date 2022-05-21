38d24b58ac9cba2951486134675fb958bfe7ff4a
Kazi <kazinoshin.jo@gmail.com>
2022-05-21 15:31:20 +0600

    Add a named agent and fix broken Dictionary.random_word API

--- dictionary/lib/runtime/server.ex
+++ dictionary/lib/runtime/server.ex
@@ -1,14 +1,15 @@
 defmodule Dictionary.Runtime.Server do
   @type t :: pid()
 
+  @me __MODULE__
   alias Dictionary.Impl.Words
 
   @spec start_link() :: {:ok, t}
   def start_link() do
-    Agent.start_link(&Words.word_list/0)
+    Agent.start_link(&Words.word_list/0, name: @me)
   end
 
-  def random_word(pid) do
-    Agent.get(pid, &Words.random_word/1)
+  def random_word() do
+    Agent.get(@me, &Words.random_word/1)
   end
 end
