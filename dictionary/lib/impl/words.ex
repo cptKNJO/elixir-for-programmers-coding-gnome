0d6d74cd9a4b8ea392628489e2121c0d2e7ea1c6
Kazi <kazinoshin.jo@gmail.com>
2022-05-25 18:19:19 +0600

    Fix path issue

--- dictionary/lib/impl/words.ex
+++ dictionary/lib/impl/words.ex
@@ -2,21 +2,17 @@ defmodule Dictionary.Impl.Words do
   @type t :: list(String.t())
 
   @spec word_list() :: t
-  def word_list() do
-    "assets/words.txt"
-    |> get_words_from_file()
+
+  def word_list do
+    "../../assets/words.txt"
+    |> Path.expand(__DIR__)
+    |> File.read!()
+    |> String.split(~r/\n/, trim: true)
   end
 
   @spec random_word(t) :: String.t()
   def random_word(word_list) do
     word_list
     |> Enum.random()
-  end
-
-  @spec get_words_from_file(String.t()) :: t
-  defp get_words_from_file(filename) do
-    filename
-    |> File.read!()
-    |> String.split(~r/\n/, trim: true)
   end
 end
