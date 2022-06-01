915e142a46f5e5c8986236e638ebec7b1d7e0698
Kazi <kazinoshin.jo@gmail.com>
2022-06-01 16:55:54 +0600

    Add update route which lets user make move

--- b1/lib/b1_web/controllers/hangman_controller.ex
+++ b1/lib/b1_web/controllers/hangman_controller.ex
@@ -9,8 +9,18 @@ defmodule B1Web.HangmanController do
     game = Hangman.new_game()
     tally = Hangman.tally(game)
 
-    put_session(conn, :game, game)
+    conn
+    |> put_session(:game, game)
+    |> render("new.html", tally: tally)
+  end
 
-    render(conn, "new.html", tally: tally)
+  def update(conn, %{"make_move" => make_move}) do
+    tally =
+      conn
+      |> get_session(:game)
+      |> Hangman.make_move(make_move["guess"])
+
+    put_in(conn.params["make_move"]["guess"], "")
+    |> render("new.html", tally: tally)
   end
 end
